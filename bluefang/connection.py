d7ebf030ec31f22345d2a7b3c524189d8374da70
Todd McNeal <todd@curalate.com>
2017-01-21 17:13:14 -0500

    it works!

--- bluefang/connection.py
+++ bluefang/connection.py
@@ -4,6 +4,9 @@ import bluetooth
 import dbus
 import dbus.mainloop.glib
 from gi.repository import GObject as gobject
+from queue import *
+import sys
+import time
 from threading import Thread
 
 from bluefang import agents
@@ -18,6 +21,8 @@ BLUEZ_PROFILE_MANAGER = BLUEZ_SERVICE + ".ProfileManager1"
 HID_UUID = "00001124-0000-1000-8000-00805f9b34fb"
 HID_CONTROL_PSM = 17
 HID_INTERRUPT_PSM = 19
+
+q = Queue()
 
 class Bluefang():
     def __init__(self):
@@ -54,10 +59,12 @@ class Bluefang():
     def start_server(self):
         control_server_socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
         control_server_socket.bind(("", HID_CONTROL_PSM))
+        bluetooth.set_l2cap_mtu(control_server_socket, 64)
         control_server_socket.listen(1)
 
         interrupt_server_socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
         interrupt_server_socket.bind(("", HID_INTERRUPT_PSM))
+        bluetooth.set_l2cap_mtu(interrupt_server_socket, 64)
         interrupt_server_socket.listen(1)
 
         print("Listening on both PSMs")
@@ -74,6 +81,11 @@ class Bluefang():
         interrupt = L2CAPWorker(interrupt_socket, interrupt_address, 'send')
         interrupt.daemon = True
         interrupt.start()
+
+        while 1:
+            command = input("Enter a command: ")
+            q.put(command)
+
 
     def disconnect(self, blah):
         print("DISCONNECT %s" % blah) #TODO implement this
@@ -128,22 +140,50 @@ class L2CAPWorker(Thread):
         self.sendOrReceive = sendOrReceive
 
     def run(self):
-        size = 1
+        size = 500
         try:
-            while 1:
-                if self.sendOrReceive == 'send':
-                    print("Sending on address %s" % self.address)
-                    client.send(input("Enter message: "))
-                else:
-                    print("Listening on address %s" % self.address)
-                    data = client.recv(size)
-                    if data:
-                        print("PSM: " + data)
-                        client.send(data)
+            if self.sendOrReceive == 'send':
+                print("Sending on address: ${0}".format(self.address))
+                while True:
+                    command = q.get()
+                    print("Received command: " + command)
+                    if command == "left":
+                        self.socket.send(bytes(bytearray((0xA1, 0x01, 0x00, 0x00, 0x50, 0x00, 0x00, 0x00, 0x00, 0x00))))
+                        self.socket.send(bytes(bytearray((0xA1, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00))))
+                    elif command == "right":
+                        self.socket.send(bytes(bytearray((0xA1, 0x01, 0x00, 0x00, 0x4F, 0x00, 0x00, 0x00, 0x00, 0x00))))
+                        self.socket.send(bytes(bytearray((0xA1, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00))))
+                    elif command == "up":
+                        self.socket.send(bytes(bytearray((0xA1, 0x01, 0x00, 0x00, 0x52, 0x00, 0x00, 0x00, 0x00, 0x00))))
+                        self.socket.send(bytes(bytearray((0xA1, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00))))
+                    elif command == "down":
+                        self.socket.send(bytes(bytearray((0xA1, 0x01, 0x00, 0x00, 0x51, 0x00, 0x00, 0x00, 0x00, 0x00))))
+                        self.socket.send(bytes(bytearray((0xA1, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00))))
+                    elif command == "enter":
+                        self.socket.send(bytes(bytearray((0xA1, 0x01, 0x00, 0x00, 0x28, 0x00, 0x00, 0x00, 0x00, 0x00))))
+                        self.socket.send(bytes(bytearray((0xA1, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00))))
                     else:
-                        print("NO DATA")
-        except:
-            print("Closing socket")
+                        print("Unknown command: " + command)
+                    q.task_done()
+
+                    #self.socket.send(chr(0xA1) + chr(0x00) + chr(0x00) + chr(0x00) + chr(0x00))
+                    #self.socket.send(chr(0xA1) + chr(0x54)) #up
+                    #self.socket.send(chr(0xA1) + chr(0x55)) #right
+                    #self.socket.send(chr(0xA1) + chr(0x56)) #down
+                    #self.socket.send(chr(0xA1) + chr(0x57)) #left
+            else:
+                print("Receiving on address: ${0}".format(self.address))
+                while 1:
+                    data = self.socket.recv(size)
+                    if data:
+                        print("Received data:")
+                        print(':'.join(hex(x) for x in data))
+                        if data[0] == 0x71:
+                            print("Server wants to use Report Protocol Mode. Acknowledging.")
+                            self.socket.send(chr(0x00)) # Acknowledge that we will use this protocol mode
+                            self.socket.send(chr(0xA1) + chr(0x04))
+        finally:
+            print("Closing socket for address: ${0}".format(self.address))
             #client.close()
             self.socket.close()
 
