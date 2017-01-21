23d0b24f2348a45fdaf1286133ef079ee8cb8e1b
Todd McNeal <todd@curalate.com>
2017-01-21 12:06:12 -0500

    more futzing

--- bluefang/connection.py
+++ bluefang/connection.py
@@ -4,6 +4,7 @@ import bluetooth
 import dbus
 import dbus.mainloop.glib
 from gi.repository import GObject as gobject
+from threading import Thread
 
 from bluefang import agents
 from bluefang import servicerecords
@@ -38,32 +39,41 @@ class Bluefang():
         Attempt to connect to the given Device.
         """
 
+        socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+        print("Attempting to connect to device %s" % deviceAddress)
+        socket.connect((deviceAddress, 0x1001))
+        print("Connected! Spawning thread")
+        connection = L2CAPWorker(socket, deviceAddress)
+        connection.daemon = True
+        connection.start()
         # Device Connect
-        devicePath = '/org/bluez/hci0/dev_' + deviceAddress.replace(':', '_')
-        device = dbus.Interface(self.bus.get_object(BLUEZ_SERVICE, devicePath), BLUEZ_DEVICE)
-        device.Connect()
+        #devicePath = '/org/bluez/hci0/dev_' + deviceAddress.replace(':', '_')
+        #device = dbus.Interface(self.bus.get_object(BLUEZ_SERVICE, devicePath), BLUEZ_DEVICE)
+        #device.Connect()
 
     def start_server(self):
-        control_socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
-        control_socket.bind(("", HID_CONTROL_PSM))
-        control_socket.listen(1)
-        interrupt_socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
-        interrupt_socket.bind(("", HID_INTERRUPT_PSM))
-        interrupt_socket.listen(1)
+        control_server_socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+        control_server_socket.bind(("", HID_CONTROL_PSM))
+        control_server_socket.listen(1)
 
-        control_socket, control_info = control_socket.accept()
-        interrupt_socket, interrupt_info = interrupt_socket.accept()
+        interrupt_server_socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+        interrupt_server_socket.bind(("", HID_INTERRUPT_PSM))
+        interrupt_server_socket.listen(1)
 
-        print("L2CAP connection established")
+        print("Listening on both PSMs")
 
-  """
-  Every message should have the following:
+        control_socket, control_address = control_server_socket.accept()
+        interrupt_socket, interrupt_address = interrupt_server_socket.accept()
 
-  L2CAP Length (2 bytes)
-  L2CAP CID (2 bytes)
-  HIDP Header (1 byte)
-  HID Payload (variable)
-  """
+        print("Spawned control and interrupt connection threads")
+
+        control = L2CAPWorker(control_socket, control_address, 'receive')
+        control.daemon = True
+        control.start()
+
+        interrupt = L2CAPWorker(interrupt_socket, interrupt_address, 'send')
+        interrupt.daemon = True
+        interrupt.start()
 
     def disconnect(self, blah):
         print("DISCONNECT %s" % blah) #TODO implement this
@@ -109,3 +119,39 @@ class Bluefang():
     def isConnectionEstablished(self):
         print("Connection established?")
         return True
+
+class L2CAPWorker(Thread):
+    def __init__(self, socket, address, sendOrReceive):
+        Thread.__init__(self)
+        self.socket = socket
+        self.address = address
+        self.sendOrReceive = sendOrReceive
+
+    def run(self):
+        size = 1
+        try:
+            while 1:
+                if self.sendOrReceive == 'send':
+                    print("Sending on address %s" % self.address)
+                    client.send(input("Enter message: "))
+                else:
+                    print("Listening on address %s" % self.address)
+                    data = client.recv(size)
+                    if data:
+                        print("PSM: " + data)
+                        client.send(data)
+                    else:
+                        print("NO DATA")
+        except:
+            print("Closing socket")
+            #client.close()
+            self.socket.close()
+
+"""
+Every message should have the following:
+
+L2CAP Length (2 bytes)
+L2CAP CID (2 bytes)
+HIDP Header (1 byte)
+HID Payload (variable)
+"""
