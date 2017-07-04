6f8732908d043db2179363853a50ee0839c91dff
Todd McNeal <todd.mcneal@gmail.com>
2017-07-04 17:00:58 -0400

    Require Python 3.4 or above

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     keywords = ['bluetooth', 'hid', 'l2cap', 'bluez', 'pybluez', 'dbus'],
     url='https://www.github.com/tmcneal/bluefang',
     packages=find_packages(exclude=['scripts', 'tests']),
+    python_requires='~=3.4',
     install_requires=[
         "dbus-python==1.2.4",
         "PyBluez==0.22.1",
