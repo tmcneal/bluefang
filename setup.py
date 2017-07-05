f818e7117ccb9a5e67d7e87a38d7b2ca2f23737c
Todd McNeal <todd.mcneal@gmail.com>
2017-07-04 21:22:31 -0400

    Fix setup.py so it installs successfully on Ubuntu

--- setup.py
+++ setup.py
@@ -5,25 +5,23 @@ import sys
 
 setup(
     name='bluefang',
-    version='0.1',
+    version='0.1.1',
     description='Bluetooth and HID utilities for Python 3',
     author='Todd McNeal',
     author_email='todd.mcneal@gmail.com',
     keywords = ['bluetooth', 'hid', 'l2cap', 'bluez', 'pybluez', 'dbus'],
     url='https://www.github.com/tmcneal/bluefang',
     packages=find_packages(exclude=['scripts', 'tests']),
-    python_requires='~=3.4',
+    python_requires='>=3.4',
     install_requires=[
-        "dbus-python==1.2.4",
-        "PyBluez==0.22.1",
-        "vext.gi==0.5.20"
-    ] + ([
-        "lightblue==0.4.1",
-        "pyobjc==3.2.1"
-    ] if sys.platform == 'darwin' else []
-    ),
+        'dbus-python==1.2.4',
+        'PyBluez==0.22.1',
+        'vext.gi==0.5.20',
+        'lightblue==0.4.1;platform_system=="Darwin"',
+        'pyobjc==3.2.1;platform_system=="Darwin"'
+    ],
     dependency_links=[
-        'git+ssh://git@github.com/tmcneal/pybluez.git@e1311bd83447a641d59a36c8c32d8e3f75e1a34c#egg=PyBluez-0.22.1',
+        'git+https://github.com/tmcneal/pybluez@e1311bd83447a641d59a36c8c32d8e3f75e1a34c#egg=PyBluez-0.22.1',
         'git+https://github.com/tmcneal/lightblue-0.4@7575c17bb439029436cba5c0eeba3d745c3361f1#egg=lightblue-0.4.1'
     ]
 )
